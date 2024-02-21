ad981ed73f97aa3d9b54afa2557eb63a99f3f4e5
Vishwas Sharma <ch0nky@protonmail.com>
2024-02-21 12:22:28 +0530

    Initial Commit

---
+++ DungeonsAndDice/DungeonsAndDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonsAndDiceApp.swift
+//  DungeonsAndDice
+//
+//  Created by Vishwas Sharma on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DungeonsAndDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
